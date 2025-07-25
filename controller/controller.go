97574c29ac4a65eae74899ea6d048da903c69fa1
Rolly Maulana Awangga <awangga@gmail.com>
2025-07-25 12:10:58 +0700

    404

--- controller/controller.go
+++ controller/controller.go
@@ -12,6 +12,6 @@ func GetHome(w http.ResponseWriter, r *http.Request) {
 
 func NotFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write([]byte(`{"status":"success","message":"Welcome"}`))
+	w.WriteHeader(404)
+	w.Write([]byte(`{"status":"Not Found","message":"Page Not FOund"}`))
 }
